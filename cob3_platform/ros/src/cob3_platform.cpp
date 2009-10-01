a9a14a1a081053eca1f99377b6fbc950940bc111
cob <cob@cob3-2-PC4.ipa.fhg.de>
2009-10-01 18:22:05 +0200

    cob3_platform: added odometry message to publish

--- cob3_platform/ros/src/cob3_platform.cpp
+++ cob3_platform/ros/src/cob3_platform.cpp
@@ -9,7 +9,8 @@
 
 // ROS message includes
 #include <geometry_msgs/Twist.h>
-#include <geometry_msgs/Pose2D.h>
+#include <tf/transform_broadcaster.h>
+#include <nav_msgs/Odometry.h>
 
 // ROS service includes
 #include <cob3_srvs/Init.h>
@@ -30,7 +31,9 @@ class NodeClass
                 
         // topics to publish
         ros::Publisher topicPub_Pose2D;
-        
+        ros::Publisher topicPub_Odometry;
+        tf::TransformBroadcaster odom_broadcaster;
+
 	    // topics to subscribe, callback is called for new messages arriving
         ros::Subscriber topicSub_CmdVel;
         
@@ -46,17 +49,26 @@ class NodeClass
         // global variables
         PlatformHardware* pltf;
         bool isInitialized;
-        double velX,velY,angZ;
+        double cmdVelX, cmdVelY, cmdVelTh;
+        double x, y, th;
+        double dx, dy, dth, dvth;
+        double vx, vy, vth, vvth;
+        ros::Time current_time;
 
         // Constructor
         NodeClass()
-        {        	
+        {
+            // initialize global variables
             isInitialized = false;
-            velX = velY = angZ = 0;
+            current_time = ros::Time::now();
+            cmdVelX = cmdVelY = cmdVelTh = 0;
+            x = y = th = 0;
+            dx = dy = dth = dvth = 0;
+            vx = vy = vth = vvth = 0;
 
         	// implementation of topics to publish
-            topicPub_Pose2D = n.advertise<geometry_msgs::Pose2D>("cob3/platform/Pose2D", 1);
-            
+            topicPub_Odometry = n.advertise<nav_msgs::Odometry>("cob3/platform/Odometry", 50);
+
             // implementation of topics to subscribe
             topicSub_CmdVel = n.subscribe("cob3/platform/CmdVel", 1, &NodeClass::topicCallback_CmdVel, this);
             
@@ -79,12 +91,12 @@ class NodeClass
         // function will be called when a new message arrives on a topic
         void topicCallback_CmdVel(const geometry_msgs::Twist::ConstPtr& msg)
         {
-            ROS_INFO("received new velocity command [linX=%3.5f,linY=%3.5f,angZ=%3.5f]", 
+            ROS_INFO("received new velocity command [cmdVelX=%3.5f,cmdVelY=%3.5f,cmdVelTh=%3.5f]", 
                      msg->linear.x, msg->linear.y, msg->angular.z);
 
-            velX = msg->linear.x;
-            velY = msg->linear.y;
-            angZ = msg->angular.z;
+            cmdVelX = msg->linear.x;
+            cmdVelY = msg->linear.y;
+            cmdVelTh = msg->angular.z;
         }
 
         // service callback functions
@@ -92,58 +104,111 @@ class NodeClass
         bool srvCallback_Init(cob3_srvs::Init::Request &req,
                               cob3_srvs::Init::Response &res )
         {
-            ROS_INFO("This is srvCallback_Init");
-            pltf = new PlatformHardware();
-            pltf->initPltf();
-            isInitialized = true;
-            res.success = 0; // 0 = true, else = false
+            if(isInitialized == false)
+            {
+                ROS_INFO("This is srvCallback_Init");
+                pltf = new PlatformHardware();
+                pltf->initPltf();
+                isInitialized = true;
+                res.success = 0; // 0 = true, else = false
+            }
+            res.success = 1;
             return true;
         }
         
         bool srvCallback_Stop(cob3_srvs::Stop::Request &req,
                               cob3_srvs::Stop::Response &res )
         {
-            ROS_INFO("This is srvCallback_Stop");
-            velX = 0;
-            velY = 0;
-            angZ = 0;
-            res.success = 0; // 0 = true, else = false
+            if(isInitialized == true)
+            {
+                ROS_INFO("This is srvCallback_Stop");
+                cmdVelX = 0;
+                cmdVelY = 0;
+                cmdVelTh = 0;
+                res.success = 0; // 0 = true, else = false
+            }
+            res.success = 1;            
             return true;
         }
 
         bool srvCallback_Shutdown(cob3_srvs::Shutdown::Request &req,
                                   cob3_srvs::Shutdown::Response &res )
         {
-            ROS_INFO("This is srvCallback_Shutdown");
-            pltf->shutdownPltf();
-            isInitialized = false;
-            res.success = 0; // 0 = true, else = false
+            if(isInitialized == true)
+            {
+                ROS_INFO("This is srvCallback_Shutdown");
+                pltf->shutdownPltf();
+                isInitialized = false;
+                res.success = 0; // 0 = true, else = false
+            }
+            res.success = 1;
             return true;
         }
         
         // other function declarations
-        void updateVelPltf()
+        void updateCmdVel()
         {
             // send vel if platform is initialized
             if(isInitialized == true)
             {
                 ROS_INFO("update vel");
-                pltf->setVelPltf(velX, velY, angZ, 0);
+                pltf->setVelPltf(cmdVelX, cmdVelY, cmdVelTh, 0);
             }
         }
 
-        void publishPose2D()
+        void updateOdometry()
         {
-            // create message
-            geometry_msgs::Pose2D msg;
-            //TODO fill message
             if(isInitialized == true)
             {
-                // publish message
-                ROS_INFO("published Pose2D");
+                // get odometry from platform
+                pltf->getDeltaPosePltf(dx, dy, dth, dvth,
+        					           vx, vy, vth, vvth);
+
+                // add delta values to old values
+                x += dx;
+                y += dy;
+                th += dth;
+
+                // since all odometry is 6DOF we'll need a quaternion created from yaw
+                geometry_msgs::Quaternion odom_quat = tf::createQuaternionMsgFromYaw(th);
+
+                //first, we'll publish the transform over tf
+                geometry_msgs::TransformStamped odom_trans;
+                odom_trans.header.stamp = current_time;
+                odom_trans.header.frame_id = "odom";
+                odom_trans.child_frame_id = "base_link";
+
+                odom_trans.transform.translation.x = x;
+                odom_trans.transform.translation.y = y;
+                odom_trans.transform.translation.z = 0.0;
+                odom_trans.transform.rotation = odom_quat;
+
+                //send the transform
+                odom_broadcaster.sendTransform(odom_trans);
+
+                //next, we'll publish the odometry message over ROS
+                nav_msgs::Odometry odom;
+                odom.header.stamp = current_time;
+                odom.header.frame_id = "odom";
+
+                //set the position
+                odom.pose.pose.position.x = x;
+                odom.pose.pose.position.y = y;
+                odom.pose.pose.position.z = 0.0;
+                odom.pose.pose.orientation = odom_quat;
+
+                //set the velocity
+                odom.child_frame_id = "base_link";
+                odom.twist.twist.linear.x = vx;
+                odom.twist.twist.linear.y = vy;
+                odom.twist.twist.angular.z = vth;
+
+                //publish the message
+                topicPub_Odometry.publish(odom);
+
+                ROS_INFO("published Pose2D: pos[x=%3.2f,y=%3.2f,th=%3.2f]",x, y, th);
+                ROS_INFO("published Pose2D: vel[vy=%3.2f,vx=%3.2f,vth=%3.2f]",vx, vy, vth);
             }
-            
-            topicPub_Pose2D.publish(msg);
         }
 };
 
@@ -160,11 +225,13 @@ int main(int argc, char** argv)
  	ros::Rate loop_rate(10); // Hz 
     while(nodeClass.n.ok())
     {
-        // publish Pose2D
-        nodeClass.publishPose2D();
+        nodeClass.current_time = ros::Time::now();
+
+        // publish Odometry
+        nodeClass.updateOdometry();
 
         // update velocity of platform
-        nodeClass.updateVelPltf();
+        nodeClass.updateCmdVel();
 
         ros::spinOnce();
         loop_rate.sleep();
