d132e0eabdd38a2df26e5e34bc0927d37e1198eb
fmw <mmb-weisshardtf@ipa.fhg.de>
2009-10-15 13:46:59 +0200

    sickS300: first Version

---
+++ sickS300/ros/src/sickS300.cpp
@@ -0,0 +1,117 @@
+//##################
+//#### includes ####
+
+// standard includes
+//--
+
+// ROS includes
+#include <ros/ros.h>
+
+// ROS message includes
+#include <sensor_msgs/LaserScan.h>
+
+// ROS service includes
+//--
+
+// external includes
+//--
+
+//####################
+//#### node class ####
+class NodeClass
+{
+    //
+    public:
+	    // create a handle for this node, initialize node
+	    ros::NodeHandle n;
+                
+        // topics to publish
+        ros::Publisher topicPub_LaserScan;
+        
+	    // topics to subscribe, callback is called for new messages arriving
+		//--
+        
+        // service servers
+        //--
+            
+        // service clients
+        //--
+        
+        // global variables
+        //--
+
+        // Constructor
+        NodeClass()
+        {
+            topicPub_LaserScan = n.advertise<sensor_msgs::LaserScan>("LaserScan", 1);
+        }
+        
+        // Destructor
+        ~NodeClass() 
+        {
+        }
+
+        // topic callback functions 
+        // function will be called when a new message arrives on a topic
+		//--
+
+        // service callback functions
+        // function will be called when a service is querried
+        //--
+        
+        // other function declarations
+		void publishLaserScan()
+        {
+        	// fill message
+        	int num_readings = 100; //TODO
+			double laser_frequency = 40; //TODO
+			
+        	sensor_msgs::LaserScan laserScan;
+			laserScan.header.stamp = ros::Time::now();
+			laserScan.header.frame_id = "laser_frame";
+			laserScan.angle_min = -1.57; //TODO
+			laserScan.angle_max = 1.57; //TODO
+			laserScan.angle_increment = 3.14 / num_readings; //TODO
+			laserScan.time_increment = (1 / laser_frequency) / (num_readings); //TODO
+			laserScan.range_min = 0.0; //TODO
+			laserScan.range_max = 100.0; //TODO
+
+   			laserScan.set_ranges_size(num_readings);
+    		laserScan.set_intensities_size(num_readings);
+			for(int i = 0; i < num_readings; ++i)
+			{
+			    laserScan.ranges[i] = 10;//rand() % 100 + 1;
+			    laserScan.intensities[i] = 100;//rand() % 100 + 1;
+			}
+        
+        	// publish message
+        	ROS_INFO("published new LaserScan message");
+            topicPub_LaserScan.publish(laserScan);
+        }
+};
+
+//#######################
+//#### main programm ####
+int main(int argc, char** argv)
+{
+    // initialize ROS, spezify name of node
+    ros::init(argc, argv, "sickS300");
+    
+    NodeClass nodeClass;
+ 
+	ros::Rate loop_rate(1); // Hz
+    while(nodeClass.n.ok())
+    {
+    	// publish LaserScan
+        nodeClass.publishLaserScan();
+
+        // sleep and waiting for messages, callbacks    
+        ros::spinOnce();
+        loop_rate.sleep();
+    }
+    return 0;
+}
+
+//##################################
+//#### function implementations ####
+//--
---
+++ sickS300/ros/src/sickS300_test.cpp
@@ -0,0 +1,92 @@
+//##################
+//#### includes ####
+
+// standard includes
+//--
+
+// ROS includes
+#include <ros/ros.h>
+
+// ROS message includes
+#include <sensor_msgs/LaserScan.h>
+
+// ROS service includes
+//--
+
+// external includes
+//--
+
+//####################
+//#### node class ####
+class NodeClass
+{
+    //
+    public:
+	    // create a handle for this node, initialize node
+	    ros::NodeHandle n;
+                
+        // topics to publish
+		//--
+		        
+	    // topics to subscribe, callback is called for new messages arriving
+        ros::Subscriber topicSub_LaserScan;
+        
+        // service servers
+        //--
+            
+        // service clients
+        //--
+        
+        // global variables
+        //--
+
+        // Constructor
+        NodeClass()
+        {
+            topicSub_LaserScan = n.subscribe("LaserScan", 1, &NodeClass::topicCallback_LaserScan, this);
+        }
+        
+        // Destructor
+        ~NodeClass() 
+        {
+        }
+
+        // topic callback functions 
+        // function will be called when a new message arrives on a topic
+        void topicCallback_LaserScan(const sensor_msgs::LaserScan::ConstPtr& msg)
+        {
+            ROS_INFO("this is topicCallback_LaserScan");
+            ROS_INFO("...received new scan. ranges[1]=%f",msg->ranges[1]);
+        }
+
+        // service callback functions
+        // function will be called when a service is querried
+        //--
+        
+        // other function declarations
+		//--
+};
+
+//#######################
+//#### main programm ####
+int main(int argc, char** argv)
+{
+    // initialize ROS, spezify name of node
+    ros::init(argc, argv, "sickS300_test");
+    
+    NodeClass nodeClass;
+ 
+    while(nodeClass.n.ok())
+    {
+
+        ros::spinOnce();
+    }
+    
+//    ros::spin();
+
+    return 0;
+}
+
+//##################################
+//#### function implementations ####
+//--
