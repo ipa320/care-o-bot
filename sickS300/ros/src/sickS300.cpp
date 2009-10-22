9b91c55ea2227de03ae9691d9f6866d2f845ee93
fmw <mmb-weisshardtf@ipa.fhg.de>
2009-10-22 10:31:38 +0200

    sickS300: first working version of S300 scanner

--- sickS300/ros/src/sickS300.cpp
+++ sickS300/ros/src/sickS300.cpp
@@ -14,7 +14,7 @@
 //--
 
 // external includes
-//--
+#include <include/ScannerSickS300.h>
 
 //####################
 //#### node class ####
@@ -60,33 +60,39 @@ class NodeClass
         //--
         
         // other function declarations
-		void publishLaserScan()
+		void publishLaserScan(std::vector<double> vdDistM, std::vector<double> vdAngRAD, std::vector<double> vdIntensAU)
         {
         	// fill message
-        	int num_readings = 100; //TODO
-			double laser_frequency = 40; //TODO
+        	int num_readings = vdDistM.size();
+			double laser_frequency = 10; //TODO: read from Ini-file
 			
+			// create LaserScan Data-Container
         	sensor_msgs::LaserScan laserScan;
+
+			// get time stamp for header
 			laserScan.header.stamp = ros::Time::now();
-			laserScan.header.frame_id = "base_link";
-			laserScan.angle_min = -1.57; //TODO
-			laserScan.angle_max = 1.57; //TODO
-			laserScan.angle_increment = 3.14 / num_readings; //TODO
-			laserScan.time_increment = (1 / laser_frequency) / (num_readings); //TODO
-			laserScan.range_min = 0.0; //TODO
-			laserScan.range_max = 100.0; //TODO
+            
+			// set scan parameters
+			laserScan.header.frame_id = "base_laser"; 
+			laserScan.angle_min = vdAngRAD[0]; // first ScanAngle
+			laserScan.angle_max = vdAngRAD[num_readings - 1]; // last ScanAngle
+			laserScan.angle_increment = vdAngRAD[1] - vdAngRAD[0];
+			laserScan.range_min = 0.0; //TODO read from ini-file/parameter-file
+			laserScan.range_max = 100.0; //TODO read from ini-file/parameter-file
+
+			laserScan.time_increment = (1 / laser_frequency) / (num_readings); //TODO read from ini-file/parameter-file
 
    			laserScan.set_ranges_size(num_readings);
     		laserScan.set_intensities_size(num_readings);
-			for(int i = 0; i < num_readings; ++i)
+			for(int i = 0; i < num_readings; i++)
 			{
-			    laserScan.ranges[i] = (rand() % 100 + 1) / 100.0 + 1;
-			    laserScan.intensities[i] = i;
+			    laserScan.ranges[i] = vdDistM[i];
+			    laserScan.intensities[i] = vdIntensAU[i];
 			}
         
         	// publish message
-        	ROS_INFO("published new LaserScan message");
             topicPub_LaserScan.publish(laserScan);
+        	ROS_INFO("published new LaserScan message");
         }
 };
 
@@ -98,12 +104,41 @@ int main(int argc, char** argv)
     ros::init(argc, argv, "sickS300");
     
     NodeClass nodeClass;
+	ScannerSickS300 SickS300;
+
+	//char *pcPort = new char();
+	const char pcPort[] = "/dev/ttyUSB0";
+	//(*pcPort) = "/dev/ttyUSB0";
+	int iBaudRate = 500000;
+	bool bOpenScan, bRecScan;
+	std::vector<double> vdDistM, vdAngRAD, vdIntensAU;
  
-	ros::Rate loop_rate(1); // Hz
+	bOpenScan = SickS300.open(pcPort, iBaudRate);
+	if(bOpenScan)
+		ROS_INFO("Scanner opened successfully");
+	else
+	{
+		ROS_ERROR("Scanner not available");
+		return 0;
+	}
+
+	ros::Rate loop_rate(5); // Hz
     while(nodeClass.n.ok())
     {
+		// read scan
+		ROS_INFO("Read Scanner!");
+		bRecScan = SickS300.getScan(vdDistM, vdAngRAD, vdIntensAU);
+		ROS_INFO("Scanner read successfully");
     	// publish LaserScan
-        nodeClass.publishLaserScan();
+        if(bRecScan)
+        {
+		    ROS_INFO("publish LaserScan message!");
+            nodeClass.publishLaserScan(vdDistM, vdAngRAD, vdIntensAU);
+        }
+        else
+        {
+		    ROS_INFO("No Scan available");
+        }
 
         // sleep and waiting for messages, callbacks    
         ros::spinOnce();
