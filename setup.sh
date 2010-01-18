7b2cc7e50494a975688411d78bd290b43fea83b4
ipa-fmw <mmb-weisshardtf@ipa.fhg.de>
2010-01-18 13:58:28 +0100

    auto add for evnironment variables through bashrc

--- setup.sh
+++ setup.sh
@@ -4,13 +4,12 @@
 # Florian Weisshardt, mail:florian.weisshardt@ipa.fhg.de
 #
 # Description:
-# This file sets the ros environment variables
+# This file sets the ros environment variables. Executed 
+# manually by makeconfig or automatically through bashrc
 
-# add path for cob-ros-pkg to $ROS_PACKAGE_PATH
-export ROOT=$(pwd)
-export ROS_PACKAGE_PATH=$ROS_PACKAGE_PATH:$ROOT/.
-export ROS_STACK_PATH=$ROS_STACK_PATH:$ROOT/.
-export PYTHONPATH=$PYTHONPATH:$ROS_ROOT/core/roslib/src
+# add path to $ROS_PACKAGE_PATH and $ROS_STACK_PATH
+export ROS_PACKAGE_PATH=$ROS_PACKAGE_PATH:$1
+export ROS_STACK_PATH=$ROS_STACK_PATH:$1
 
 # define amount of ros prozesses during build for multi-prozessor machines
 export ROS_PARALLEL_JOBS=-j2
