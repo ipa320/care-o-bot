12164d964751960c4642924e8f8ac504f44e139d
ipa-fmw <mmb-weisshardtf@ipa.fhg.de>
2010-01-19 16:35:20 +0100

    added LGPL licence header

--- cob3_driver/libCameraSensors/common/include/AbstractColorCamera.h
+++ cob3_driver/libCameraSensors/common/include/AbstractColorCamera.h
@@ -1,7 +1,55 @@
-/// @file AbstractColorCamera.h
-/// Abstract interface for color cameras.
-/// @author Jan Fischer
-/// @date May 2008.
+/****************************************************************
+ *
+ * Copyright (c) 2010
+ *
+ * Fraunhofer Institute for Manufacturing Engineering	
+ * and Automation (IPA)
+ *
+ * +++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+ *
+ * Project name: care-o-bot
+ * ROS stack name: cob3_driver
+ * ROS package name: libCameraSensors
+ * Description: Abstract interface for color cameras.
+ *								
+ * +++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+ *			
+ * Author: Jan Fischer, email:jan.fischer@ipa.fhg.de
+ * Supervised by: Jan Fischer, email:jan.fischer@ipa.fhg.de
+ *
+ * Date of creation: Mai 2008
+ * ToDo:
+ *
+ * +++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions are met:
+ *
+ *     * Redistributions of source code must retain the above copyright
+ *       notice, this list of conditions and the following disclaimer.
+ *     * Redistributions in binary form must reproduce the above copyright
+ *       notice, this list of conditions and the following disclaimer in the
+ *       documentation and/or other materials provided with the distribution.
+ *     * Neither the name of the Fraunhofer Institute for Manufacturing 
+ *       Engineering and Automation (IPA) nor the names of its
+ *       contributors may be used to endorse or promote products derived from
+ *       this software without specific prior written permission.
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License LGPL as 
+ * published by the Free Software Foundation, either version 3 of the 
+ * License, or (at your option) any later version.
+ * 
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Lesser General Public License LGPL for more details.
+ * 
+ * You should have received a copy of the GNU Lesser General Public 
+ * License LGPL along with this program. 
+ * If not, see <http://www.gnu.org/licenses/>.
+ *
+ ****************************************************************/
 
 #ifndef __ABSTRACTCOLORCAMERA_H__
 #define __ABSTRACTCOLORCAMERA_H__
