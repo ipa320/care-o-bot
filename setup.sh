5101c50adb15bdb1b74a6f80602fb086ff403c98
ipa-fmw <mmb-weisshardtf@ipa.fhg.de>
2010-02-10 17:51:11 +0100

    improved makeconfig to set ROS_PACKAGE_PATH

--- setup.sh
+++ setup.sh
@@ -7,9 +7,8 @@
 # This file sets the ros environment variables. Executed 
 # manually by makeconfig or automatically through bashrc
 
-# add path to $ROS_PACKAGE_PATH and $ROS_STACK_PATH
-export ROS_PACKAGE_PATH=$ROS_PACKAGE_PATH:$1
-#export ROS_STACK_PATH=$ROS_STACK_PATH:$1
+# add path to $ROS_PACKAGE_PATH
+export ROS_PACKAGE_PATH=$1:$ROS_PACKAGE_PATH
 
 # define amount of ros prozesses during build for multi-prozessor machines
 export ROS_PARALLEL_JOBS=-j2
