42e556c04aaf130a4eb930fc85f72218847581a0
Florian Weisshardt <florian.weisshardt@ipa.fraunhofer.de>
2015-11-25 17:30:59 +0100

    Update .travis.install.sh

--- .travis.install.sh
+++ .travis.install.sh
@@ -1,3 +1,6 @@
+set -e
+set -v
+
 sudo sh -c 'echo "deb http://packages.ros.org/ros/ubuntu trusty main" > /etc/apt/sources.list.d/ros-latest.list'
 wget http://packages.ros.org/ros.key -O - | sudo apt-key add -
 sudo apt-get update -qq
