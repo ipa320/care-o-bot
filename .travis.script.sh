910ba4588b417d030cb965307b0c1199ea174841
Florian Weisshardt <florian.weisshardt@ipa.fraunhofer.de>
2015-12-14 22:25:12 +0100

    do not log setup.bash outputs

--- .travis.script.sh
+++ .travis.script.sh
@@ -3,7 +3,7 @@ set -v
 
 # create empty overlay workspace
 mkdir -p $CATKIN_WS_SRC
-source $CATKIN_WS_UNDERLAY/install/setup.bash # source install space of underlay
+source $CATKIN_WS_UNDERLAY/install/setup.bash > /dev/null # source install space of underlay
 catkin_init_workspace $CATKIN_WS_SRC
 cd $CATKIN_WS
 catkin_make -DCMAKE_BUILD_TYPE=Release # build empty overlay
@@ -12,7 +12,7 @@ ln -s $TRAVIS_BUILD_DIR $CATKIN_WS_SRC
 # install dependencies from overlay
 rosdep install -q --from-paths $CATKIN_WS_SRC -i -y --rosdistro $CI_ROS_DISTRO > /dev/null
 # build overlay
-source $CATKIN_WS/devel/setup.bash # source devel space of overlay
+source $CATKIN_WS/devel/setup.bash > /dev/null # source devel space of overlay
 catkin_make -DCMAKE_BUILD_TYPE=Release
 catkin_make run_tests # test overlay
 catkin_test_results --verbose
