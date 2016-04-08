5609d70c0d35b9df9ae366bf44152a905a010e8f
Florian Weisshardt <florian.weisshardt@ipa.fraunhofer.de>
2016-04-08 13:07:45 +0200

    Update .travis.install.sh

--- .travis.install.sh
+++ .travis.install.sh
@@ -20,11 +20,11 @@ cd $CATKIN_WS_UNDERLAY
 if [ -f $TRAVIS_BUILD_DIR/.travis.rosinstall ]; then wstool init -j10 src $TRAVIS_BUILD_DIR/.travis.rosinstall; fi
 if [ ! -f $TRAVIS_BUILD_DIR/.travis.rosinstall ]; then wstool init -j10 src $DEFAULT_ROSINSTALL; fi
 # install dependencies from underlay
-rosdep install -q --from-paths $CATKIN_WS_UNDERLAY_SRC -i -y --rosdistro $CI_ROS_DISTRO > /dev/null 2>&1
+rosdep install -q --from-paths $CATKIN_WS_UNDERLAY_SRC -i -y --rosdistro $CI_ROS_DISTRO > /dev/null #2>&1
 # build devel space of underlay
 catkin_make -DCMAKE_BUILD_TYPE=Release
 # build install space of underlay
-catkin_make -DCMAKE_BUILD_TYPE=Release install > /dev/null 2>&1
+catkin_make -DCMAKE_BUILD_TYPE=Release install > /dev/null #2>&1
 ret=$?
 kill %%
 exit $ret
