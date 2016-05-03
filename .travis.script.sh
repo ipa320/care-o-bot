32e3d6ea42f85999bd0310c34957dd296855fdeb
Florian Weisshardt <florian.weisshardt@ipa.fraunhofer.de>
2016-05-03 15:09:46 +0200

    Update .travis.script.sh

--- .travis.script.sh
+++ .travis.script.sh
@@ -18,7 +18,11 @@ source $CATKIN_WS/devel/setup.bash > /dev/null 2>&1 # source devel space of over
 catkin_make -DCMAKE_BUILD_TYPE=Release
 catkin_make -DCMAKE_BUILD_TYPE=Release install > /dev/null
 source $CATKIN_WS/install/setup.bash > /dev/null 2>&1 # source install space of overlay
-catkin_make test # test overlay
+if [ "$CATKIN_ENABLE_TESTING" == "OFF" ]; then
+  echo "Testing disabled"
+else
+  catkin_make test # test overlay
+fi
 catkin_test_results --verbose
 ret=$?
 kill %%
